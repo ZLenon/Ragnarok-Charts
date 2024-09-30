812b25a38b86bfc96a6e35068fa4d6bb740b3614
Lenon Nascimento <lenoninformatica@gmail.com>
2024-09-30 15:28:14 -0300

    Feat: atualizações

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# Ragnarok
+# Boas-vindas ao repositório do projeto Ragnarok-Charts!
+
+<h2><strong>👨‍💻 O que foi desenvolvido</strong></h2><br />
+
+Neste projeto, foi desenvolvido uma página de cards do game Ragnarök Online (RO) onde podemos ver personagens e mostros da franquia, o game (RO) lançado pela primeira vez em 2002 pela empresa sul-coreana Gravity Co, criado por Lee Myung-jin, e tem influências da mitologia nórdica. 
+
+<h2><strong> 📃 Habilidades</strong></h2><br />
+
+  Neste projeto, verificamos capacidade de:
+  - Criar cards usando HTML e CSS
+  - Manipular DOM javascript
 
 [Link do Projeto](https://zlenon.github.io/Ragnarok/)
--- index.js
+++ index.js
@@ -1,70 +1,34 @@
-const urlGifPersona = document.getElementById('urlGifPersonas'); // tag IMG
-const confirmaBTN =  document.getElementById('confirma'); //bottao
-const nextBTN =  document.getElementById('avanca'); //bottao
-const backBTN = document.getElementById('retorna'); //bottao 
-const criaPersona = document.getElementById('criaPersona') // bottao favoritar
-const idPersona = document.getElementById('idPersona'); //inpult text
-const cardPersonas = document.getElementById('cardPersona'); // div das imagens favoritadas
-const name =  document.getElementById('name');
-const clase =  document.getElementById('clase');
-const atk =  document.getElementById('atk');
-const def =  document.getElementById('def');
-import { personagens } from './Model/personagens.js';
+const irbutton = document.getElementById("ir-button");
+const voltarbutton = document.getElementById("voltar-button");
 
-let valorAtual = '';
-idPersona.value = valorAtual;
+irbutton.addEventListener("click", () => {
+  const imgPrincipal = document.getElementById("imgPrincipal");
+  const imganterior = document.getElementById("imganterior");
+  const imgproximo = document.getElementById("imgproximo");
+  const numaterior = document.getElementById("numaterior");
+  const numatual = document.getElementById("numatual");
+  const numproximo = document.getElementById("numproximo");
 
-const search = () => {
-  valorAtual = +idPersona.value;
-  let newGif = `https://static.ragnaplace.com/db/npc/gif/${valorAtual}.gif`
-  return urlGifPersona.src = newGif;   
-}
+  numaterior.innerText = Number(numaterior.innerText) + 1;
+  numatual.innerText = Number(numatual.innerText) + 1;
+  numproximo.innerText = Number(numproximo.innerText) + 1;
+  imganterior.src = `https://static.ragnaplace.com/db/npc/gif/${numaterior.innerText}.gif`;
+  imgPrincipal.src = `https://static.ragnaplace.com/db/npc/gif/${numatual.innerText}.gif`;
+  imgproximo.src = `https://static.ragnaplace.com/db/npc/gif/${numproximo.innerText}.gif`;
+});
 
-const next = () => {
-  if (valorAtual === 3510) {
-    let newGif = `https://static.ragnaplace.com/db/npc/gif/${valorAtual}.gif`;
-    idPersona.value = valorAtual;
-    return urlGifPersona.src = newGif;   
-  }
-  valorAtual += 1;
-  let newGif = `https://static.ragnaplace.com/db/npc/gif/${valorAtual}.gif`;
-  idPersona.value = valorAtual;
-  return urlGifPersona.src = newGif; 
-}
+voltarbutton.addEventListener("click", () => {
+  const imgPrincipal = document.getElementById("imgPrincipal");
+  const imganterior = document.getElementById("imganterior");
+  const imgproximo = document.getElementById("imgproximo");
+  const numaterior = document.getElementById("numaterior");
+  const numatual = document.getElementById("numatual");
+  const numproximo = document.getElementById("numproximo");
 
-const back = () => { 
-  if (valorAtual === 1) {
-    let newGif = `https://static.ragnaplace.com/db/npc/gif/${valorAtual}.gif`
-    idPersona.value = valorAtual;
-    return urlGifPersona.src = newGif;
-  }
-  valorAtual -= 1;
-  let newGif = `https://static.ragnaplace.com/db/npc/gif/${valorAtual}.gif`
-  idPersona.value = valorAtual;
-  return urlGifPersona.src = newGif;
-}
-
-const criaPersonagem = () => {
-  let cardPersonagem = {
-    id: Number(valorAtual),
-    name: name.value, // input name
-    clase: clase.value ,// input class
-    atk: Number(atk.value), // input atk
-    def: Number(def.value) , // input def
-    url: urlGifPersona.src
-  };
-  personagens.push(cardPersonagem);
- console.log(personagens);
-  const imgNPC = document.createElement("img");
-
-  personagens.map((npc) => {
-   
-    imgNPC.src = npc.url;
-    cardPersonas.appendChild(imgNPC);
-  })
-}
-
-confirmaBTN.addEventListener('click',search);
-nextBTN.addEventListener('click',next);
-backBTN.addEventListener('click', back);
-criaPersona.addEventListener('click', criaPersonagem);
+  numaterior.innerText = Number(numaterior.innerText) - 1;
+  numatual.innerText = Number(numatual.innerText) - 1;
+  numproximo.innerText = Number(numproximo.innerText) - 1;
+  imganterior.src = `https://static.ragnaplace.com/db/npc/gif/${numaterior.innerText}.gif`;
+  imgPrincipal.src = `https://static.ragnaplace.com/db/npc/gif/${numatual.innerText}.gif`;
+  imgproximo.src = `https://static.ragnaplace.com/db/npc/gif/${numproximo.innerText}.gif`;
+});
